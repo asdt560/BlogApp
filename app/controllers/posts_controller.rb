1119c67c972aa99a9d3c05456da54278a4e074bf
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-01-25 18:10:43 -0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(:author_id => @user.id)
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
     @post = Post.find(params[:id])
     @author = User.find(params[:user_id])
-    @comments = Comment.where(:post_id => params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
